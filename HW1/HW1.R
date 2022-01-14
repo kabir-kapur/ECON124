1c7a52e76d65f419c06b448999393c3d37193fd6
Kabir Kapur <kapka6700@gmail.com>
2022-01-13 16:43:43 -0800

    final commit hw1

--- HW1/HW1.R
+++ HW1/HW1.R
@@ -1,48 +1,39 @@
 library(readstata13)
 library(scales)
 
-surveyData <- read.dta13("/Users/kabirkapur/Desktop/SCHOOL/ECON124/HW1/survey.dta")
-fakeNewsData <- read.dta13("/Users/kabirkapur/Desktop/SCHOOL/ECON124/HW1/fakenews.dta")
+surveyData <- read.dta13("/Users/kabirkapur/Desktop/SCHOOL/ECON124/HW1/survey1.dta")
+fakeNewsData <- read.dta13("/Users/kabirkapur/Desktop/SCHOOL/ECON124/HW1/fakenews1.dta")
 
 # question 2
-na.omit(surveyData$fb_share)
-sum(fakeNewsData$fb_share)
-
-View(surveyData)
-View(fakeNewsData)
-
-fakeNewsBias <- table(fakeNewsData$pro)
-surveyClinton <- table(surveyData$ArticleProClinton)
-surveyTrump <- table(surveyData$ArticleProTrump)
-
-x1 <- fakeNewsBias[2] + surveyTrump[2]
-y1 <- fakeNewsBias[1] + surveyClinton[2]
 x <- sum(na.omit(fakeNewsData$fb_share[fakeNewsData$pro=="Trump"])) * 1000
-
+y <- sum(na.omit(fakeNewsData$fb_share[fakeNewsData$pro == "Clinton"])) * 1000
 
 cat(x, "pro-Trump articles were shared on Facebook, and", y, "pro-Clinton articles were shared, in this dataset.")
 
 # question 3
-snopes <- sum(fakeNewsData$buzzfeed[])
-buzzfeed <- table(fakeNewsData$buzzfeed)[2]
-politifact <- table(fakeNewsData$politifact)[2]
+snopes <- sum(fakeNewsData$snopes)
+buzzfeed <- sum(fakeNewsData$buzzfeed)
+politifact <- sum(fakeNewsData$politifact)
 
-snopes
+snopesExclusive <- sum(fakeNewsData$snopes[fakeNewsData$buzzfeed == 0 & fakeNewsData$politifact == 0])
+buzzfeedExclusive <- sum(fakeNewsData$buzzfeed[fakeNewsData$snopes == 0 & fakeNewsData$politifact == 0])
+politifactExclusive <- sum(fakeNewsData$politifact[fakeNewsData$snopes == 0 & fakeNewsData$buzzfeed == 0])
+
+cat(snopesExclusive/snopes, buzzfeedExclusive/buzzfeed, politifactExclusive/politifact)
 
 # question 4
-sumMedia <- sum(surveyData$MediaMinutesPerDay)
+cat(sum(surveyData$NewsSource_Social) / sum(surveyData$NewsSource_Web))
+meanMedia <- round(mean(surveyData$MediaMinutesPerDay)) # mean number of minutes spent consuming media
+sumMedia <- sum(surveyData$MediaMinutesPerDay) # total number of minutes spent consuming media
 
-socialMediaRate = sum(surveyData$SocialMediaMinutesPerDay) / sumMedia
+socialMediaRate <- sum(surveyData$SocialMediaMinutesPerDay[surveyData$MediaMinutesPerDay > 0]) / sum(surveyData$MediaMinutesPerDay[surveyData$MediaMinutesPerDay > 0])
 
 socialMediaPercentage <- label_percent()(socialMediaRate)
 
-cat("Respondents spent", sumMedia, "total minutes consuming media per day, and", socialMediaPercentage, "of that time on social media.")
+cat("The average respondent spent", meanMedia, "minutes per day consuming media, and", socialMediaPercentage, "of that time on social media.")
 
 # question 5
-mediaVector <- table(surveyData$MostImportantSource)
-unlist(mediaVector)
-sort(mediaVector)
+x <- tapply(rep(1, nrow(surveyData)), surveyData$MostImportantSource, sum)
 
-par(mar = c(1,1,1,1))
-barplot(mediaVector, main = "Media Consumption Preferences of Surveyed Individuals", xlab = "Preferred Media Source", ylab = "Number of Respondents", las = 2)
+barplot(sort(x), las = 2)
 
