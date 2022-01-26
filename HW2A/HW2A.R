3e9c1c09ccde0aea7dca05f1da625f111d898338
Kabir Kapur <kapka6700@gmail.com>
2022-01-26 11:48:30 -0800

    almost done with part 2A

--- HW2A/HW2A.R
+++ HW2A/HW2A.R
@@ -1,3 +1,5 @@
+library(scales)
+library(caTools)
 # question 1
 nba <- read.csv("/Users/kabirkapur/Desktop/SCHOOL/ECON124/HW2A/NBA.csv")
 
@@ -21,3 +23,36 @@ vorpSort <- fiftyG[order(-fiftyG$VORP),]
 vorpSort[1,1]
 
 # question 5
+row <- nba[nba$Player == "Stephen Curry",]
+stephPER <- row$PER
+stephTS <- row$TS
+stephVORP<- row$VORP
+
+percentPER <- (sum(nba$PER > stephPER))/nrow(nba)
+percentPER
+percentTS <- (sum(nba$TS > stephTS))/nrow(nba)
+percentTS
+percentVORP <- (sum(nba$VORP > stephVORP))/nrow(nba)
+percentVORP
+
+# question 6
+americanNBA <- nba[nba$NBA_Country == "USA",]
+americanNBA <- within(americanNBA, rm(NBA_Country))
+americanNBA
+
+testSet_size <- round(.9*nrow(americanNBA)) # abstracting testSet pseudo-object attribute pre-definition
+set.seed(777)
+
+picked = sample(seq_len(nrow(americanNBA)),size = testSet_size)
+testSet = americanNBA[picked,]
+trainingSet = americanNBA[-picked,]
+
+testSet
+trainingSet
+
+# question 7
+# part a
+olsModel <- lm(log(Salary) ~ . - Player, data = trainingSet) #DataFlair
+summary(olsModel)
+# part c
+
